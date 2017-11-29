a06c98b7e86cf941108592ddcec7c0dffe3fecf6
Luiz Gustavo Gomes Borges <luiz.g.borges95@gmail.com>
2017-11-28 23:05:59 -0200

    Replaced message lists with dictionaries of hashes to messages. Generating messages randomly. Trimming MD5.

--- main.cpp
+++ main.cpp
@@ -1,70 +1,104 @@
-#include <string> 
-#include <list> 
-#include <iostream> 
-#include <sstream> 
-#include <iterator> 
+#include <iostream>
+#include <map>
+#include <random>
+#include <set>
+#include <string>
 #include "md5.h"
 
+#define SIZE_HASH	4
+#define SIZE_M1		10000
+#define SIZE_M2		10000
+#define SIZE_M3		10000
+#define SIZE_STR	10
+
 using namespace std;
+using HashedToOriginal = map<string, string>;
 
-void filllist(int x0, int x1, list<string>& listelements, int n){
+string create_random_string();
+string cryptohash(const string& original);
+// inline void show(const HashedToOriginal& hashes);
+HashedToOriginal find_collisons(const HashedToOriginal& hashes, int n_messages);
 
-    stringstream convert;
+int main() {
+	HashedToOriginal hashes1, hashes2, hashes3;
+	string random;
 
-    for(size_t i = x0; i < x1; i++ ){
-        convert << i;
-        string result = md5(convert.str());
-        result.resize(n);
-        listelements.push_back(result); 
-    }
+	for (int i = 0; i < SIZE_M1; i++) {
+		random = create_random_string();
+		hashes1[cryptohash(random)] = random;
+	}
 
+	// show(hashes1);
+
+	hashes2 = find_collisons(hashes1, SIZE_M2);
+	// show(hashes2);
+
+	hashes3 = find_collisons(hashes2, SIZE_M3);
+	// show(hashes3);
+
+	cout << hashes3.size() << " collisions" << endl;
+	/*cout << "String 1\tString 2\tString 3\tHash" << endl;
+	for (auto i = hashes3.begin(); i != hashes3.end(); ++i) {
+		cout << hashes1[i->first] << '\t';
+		cout << hashes2[i->first] << '\t';
+		cout << hashes3[i->first] << '\t';
+		cout << i->first << endl;
+	}*/
+
+	return 0;
 }
 
-list<string> collision(list<string> m1, list<string> m2, list<string> m3){
-    list<string> result;
+string create_random_string() {
+	static set<string> created;
+	static default_random_engine generator;
 
-    for(list<string>::iterator i = m1.begin(); i != m1.end(); i++){
-        for(list<string>::iterator j = m2.begin(); j != m2.end(); j++){
-            if(*i == *j){
-                cout << "enctrou";
-                result.push_back(*i);
-            }
-        } 
-    }
+	int random = generator();
+	string str = string(SIZE_STR, ' ');
 
-    if(result.size() > 0){
-        cout << "passou" << endl;
-        for(list<string>::iterator i = m3.begin(); i != m3.end(); i++){
-            for(list<string>::iterator j = result.begin(); j != result.end(); j++){
-                if(*i == *j){
-                    result.push_back(*i);
-                }
-            } 
-        }
-    }
+	do
+		for (int i = 0; i < SIZE_STR; i++) {
+			str[i] = random % 26 + 'a';
+			random /= 26;
 
+			if (random == 0)
+				random = generator();
+		}
+	while ( !created.insert(str).second );
+	// clog << created.size() << '\r';
 
-    return result;
+	return str;
 }
 
-int main(){
+string cryptohash(const string& original) {
+	string hashed = md5(original);
+	hashed.resize(SIZE_HASH);
+	return hashed;
+}
 
-    list<string> m1;
-    list<string> m2;
-    list<string> m3;
-    list<string> resultcollision;
+/*inline void show(const HashedToOriginal& hashes) {
+	clog << "String\t\tHash" << endl;
 
-    filllist(0, 220, m1, 3);
-    filllist(200,220, m2, 3);
-    filllist(220,225, m3, 3);
-   
-    resultcollision = collision(m1, m2, m3);
-    
-    if (resultcollision.size() > 0){
-        for(list<string>::iterator s = resultcollision.begin(); s != resultcollision.end(); s++) {
-            cout << " " << *s << endl; 
-        }
-    }
-   
-    return 0;    
+	for (auto i = hashes.begin(); i != hashes.end(); ++i)
+		clog << i->second << '\t' << i->first << endl;
+
+	clog << endl;
+}*/
+
+HashedToOriginal find_collisons(const HashedToOriginal& hashes, int n_messages) {
+	HashedToOriginal collisions;
+	string hashed, random;
+
+	for (int i = 0; i < SIZE_M2; i++) {
+		try {
+			random = create_random_string();
+			hashed = cryptohash(random);
+
+			if (hashes.at(hashed) != random)
+				collisions[hashed] = random;
+		}
+
+		catch (exception& e) {}
+	}
+
+	return collisions;
 }
