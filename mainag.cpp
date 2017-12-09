753a4efa4ea59cc9a18af650525437a041d32319
Luiz Gustavo Gomes Borges <luiz.g.borges95@gmail.com>
2017-12-09 01:20:51 -0200

    Fixed generation of initial population.

--- mainag.cpp
+++ mainag.cpp
@@ -6,17 +6,17 @@ int main(){
     int parents[TCROSS];
 
     for(size_t i = 0; i < POP; i++){
-        int remaining = SIZE_M;
-        for(size_t j = 0; j < 3; j++){
+        pop[i][2] = SIZE_M;
+        for(size_t j = 0; j < 2; j++){
             int generated = random();
-            pop[i][j] = (generated % remaining) +1;
-            remaining -= pop[i][j];
+            pop[i][j] = (generated % pop[i][2]) + 1;
+            pop[i][2] -= pop[i][j];
         }
 
         // clog << pop[i][0] << '\t' << pop[i][1] << '\t' << pop[i][2] << endl ;
         pop[i][3] = execute(pop[i][0], pop[i][1], pop[i][2]);
 
-        //cout << "colisoes:" << pop[i][3] << endl;
+        // cout << "colisoes:" << pop[i][3] << endl;
     }
 
     for(size_t nger = 0; nger < NGER; nger++){
@@ -34,37 +34,38 @@ int main(){
             }
             parents[i] = maxelement(pop, tournamentelements, TOUR);
         }
-        cout << "torneio" << endl;
+        // cout << "torneio" << endl;
         for(size_t i = 1; i < TCROSS; i = i + 2){
             number = random()%10;
             //printf("%d\n",number);
             crossover(pop, pop[parents[i-1]], pop[parents[i]], i);
         }
-        cout << "crossover" << endl;
+        // cout << "crossover" << endl;
 
 
         int n1, n2, p;
 
         for(size_t i = POP; i < TPOP; i++){
-
             p = random()%100;
             n1 = random()%3;
-            n2 = random()%3;
-            while (n1 == n2) {
+
+            // do {
                 n2 = random()%3;
-            }
+            // } while (n1 == n2);
+
             if(p <= PMUT){
                 number = pop[i][n1];
                 pop[i][n1] = pop[i][n2];
                 pop[i][n2] = number;
+                // clog << pop[i][0] << '\t' << pop[i][1] << '\t' << pop[i][2] << endl ;
                 pop[i][3] = execute(pop[i][0],pop[i][1],pop[i][2]);
             }
 
         }
-        cout << "mutate" << endl;
+        // cout << "mutate" << endl;
 
         quickSort(pop, 0, TPOP - 1, 3);
-        cout << "elitismo" << endl;
+        // cout << "elitismo" << endl;
 
     }
 
