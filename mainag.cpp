8fd376719528b15a55fd7d0e9263ecc88a87de9c
Luiz Gustavo Gomes Borges <luiz.g.borges95@gmail.com>
2017-12-10 19:00:28 -0200

    Simplified outpur.

--- mainag.cpp
+++ mainag.cpp
@@ -1,75 +1,70 @@
 #include "ag.h"
 
-int main(){
+int main() {
+	int pop[POP][4];
+	int parents[TCROSS];
 
-    int pop[POP][4];
-    int parents[TCROSS];
+	for(size_t i = 0; i < POP; i++){
+		int generated;
 
-    cout << "passou" << endl;
+		pop[i][2] = SIZE_M;
 
-    for(size_t i = 0; i < POP; i++){
-        pop[i][2] = SIZE_M;
-        for(size_t j = 0; j < 2; j++){
-            cout << "passou loop" << endl;
-            int generated = random();
-            pop[i][j] = (generated % pop[i][2]) + 1;
-            pop[i][2] -= pop[i][j];
-        }
+		for(size_t j = 0; j < 2; j++){
+			generated = random();
+			pop[i][j] = (generated % pop[i][2]) + 1;
+			pop[i][2] -= pop[i][j];
+		}
 
-        // clog << pop[i][0] << '\t' << pop[i][1] << '\t' << pop[i][2] << endl ;
-        pop[i][3] = execute(pop[i][0], pop[i][1], pop[i][2]);
+		// clog << pop[i][0] << '\t' << pop[i][1] << '\t' << pop[i][2] << endl ;
+		pop[i][3] = execute(pop[i][0], pop[i][1], pop[i][2]);
+	}
 
-        // cout << "colisoes:" << pop[i][3] << endl;
-    }
-    cout << "passou" << endl;
-    for(size_t nger = 0; nger < NGER; nger++){
-        clog << "Geração " << nger << '\r';
-        int tournamentelements[TOUR], number;
+	for(size_t nger = 0; nger < NGER; nger++){
+		clog << "Geração " << nger << endl;
+		int tournamentelements[TOUR], number;
 
-        for(size_t i = 0; i < TCROSS; i++){
-            for (size_t j = 0; j < TOUR; j++) {
-                number = random()%POP;
-                while(!verify(tournamentelements, number, j)){
-                    number = random()%POP;
-                }
-                tournamentelements[j] = number;
+		// cout << "tournament" << endl;
+		for(size_t i = 0; i < TCROSS; i++){
+			for (size_t j = 0; j < TOUR; j++) {
+				number = random()%POP;
+				while(!verify(tournamentelements, number, j)){
+					number = random()%POP;
+				}
+				tournamentelements[j] = number;
 
-            }
-            parents[i] = maxelement(pop, tournamentelements, TOUR);
-        }
-        // cout << "torneio" << endl;
-        for(size_t i = 1; i < TCROSS; i = i + 2){
-            number = random()%10;
-            //printf("%d\n",number);
-            crossover(pop, pop[parents[i-1]], pop[parents[i]], i);
-        }
-        // cout << "crossover" << endl;
+			}
+			parents[i] = maxelement(pop, tournamentelements, TOUR);
+		}
 
+		// cout << "crossover" << endl;
+		for(size_t i = 1; i < TCROSS; i = i + 2){
+			// number = random()%10;
+			crossover(pop, pop[parents[i-1]], pop[parents[i]], i);
+		}
 
-        int n1, n2, p;
+		int n1, n2, p;
 
-        for(size_t i = POP; i < TPOP; i++){
-            p = random()%100;
-            n1 = random()%3;
+		// cout << "mutate" << endl;
+		for(size_t i = POP; i < TPOP; i++){
+			p = random()%100;
+			n1 = random()%3;
 
-            // do {
-                n2 = random()%3;
-            // } while (n1 == n2);
+			// do {
+				n2 = random()%3;
+			// } while (n1 == n2);
 
-            if(p <= PMUT){
-                number = pop[i][n1];
-                pop[i][n1] = pop[i][n2];
-                pop[i][n2] = number;
-                // clog << pop[i][0] << '\t' << pop[i][1] << '\t' << pop[i][2] << endl ;
-                pop[i][3] = execute(pop[i][0],pop[i][1],pop[i][2]);
-            }
+			if(p <= PMUT){
+				number = pop[i][n1];
+				pop[i][n1] = pop[i][n2];
+				pop[i][n2] = number;
+				// clog << pop[i][0] << '\t' << pop[i][1] << '\t' << pop[i][2] << endl ;
+				pop[i][3] = execute(pop[i][0],pop[i][1],pop[i][2]);
+			}
 
-        }
-        // cout << "mutate" << endl;
+		}
 
-        quickSort(pop, 0, TPOP - 1, 3);
-        // cout << "elitismo" << endl;
-
-    }
+		// cout << "elitism" << endl;
+		quickSort(pop, 0, TPOP - 1, 3);
+	}
 
 }
